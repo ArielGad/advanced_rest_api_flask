00b6a01b11900f0507b2bec3d45e0a732c12c0cc
ArielGad <ariel.gad4@gmail.com>
2020-05-10 19:45:52 +0300

    Adding the last confirmation to the user schema

--- schemas/user.py
+++ schemas/user.py
@@ -1,3 +1,5 @@
+from marshmallow import pre_dump
+
 from ma import ma
 from models.user import UserModel
 
@@ -8,11 +10,11 @@ class UserSchema(ma.ModelSchema):  # according to User model
         load_only = ('password',)  # don't show pass while dump from OBJ to dict
         dump_only = ('id', 'activated')  # not mandatory, just declare that id is created in OBJ process, i.e. when loading there is no id
 
-
-
-
-
-
+    @pre_dump
+    def _pre_dump(self, user: UserModel):
+        user.confirmation = [user.most_recent_confirmation]  # before dump, make user's confirmation property only a list of
+        # recent confirmation
+        return user
 
 
 
