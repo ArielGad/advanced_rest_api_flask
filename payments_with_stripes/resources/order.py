31ed84abc047e7546747c132071edefaf80a6ca7
ArielGad <ariel.gad4@gmail.com>
2020-05-15 18:16:51 +0300

    Using the Association Object in our Resource

--- payments_with_stripes/resources/order.py
+++ payments_with_stripes/resources/order.py
@@ -1,9 +1,11 @@
+from collections import Counter
+
 from flask import request
 from flask_restful import Resource
 
 from libs.strings import gettext
 from models.item import ItemModel
-from models.order import OrderModel
+from models.order import OrderModel, ItemsInOrder
 
 
 class Order(Resource):
@@ -14,16 +16,19 @@ class Order(Resource):
         Expect a token and a list of item ids from the request body.
         Construct an order and talk to the Strip API to make a charge.
         """
-        data = request.get_json()  # token + list of item ids
+        data = request.get_json()  # token + list of item ids  [1, 2, 3, 5, 5, 5]
         items = []
+        item_id_quantities = Counter(data["item_ids"])
 
         # Iterate over items and retrieve them from the database
-        for _id in data["item_ids"]:
+        for _id, count in item_id_quantities.most_common():  # -> [(5,3), (3,1), (2,1), (1,1)]
             item = ItemModel.find_by_id(_id)
             if not item:
                 return {"message": gettext("order_item_by_id_not_found").format(_id)}, 404
 
-            items.append(item)
+            items.append(ItemsInOrder(item_id=_id, quantity=count))
 
         order = OrderModel(items=items, status="pending")
-        order.save_to_db()  # this does not submit to Strip
+        order.save_to_db()  # this does not submit to Stripe
+
+        order.set_status("something")
